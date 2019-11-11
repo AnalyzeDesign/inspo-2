f543ea6eee0528c30e4bc44ce779b4a7166cbcaf
B <>
2019-11-11 15:52:52 -0500

    quiz 2 assignment

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,20 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    Cat.create(cat_params)
+    redirect_to cats_path
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-      root 'static_pages#index'
-    end
+  root 'static_pages#index'
+  resources :cats
+
 end
